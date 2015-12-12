d8fd3fb226cdeac23d8e428a2b830835a7f69435
Andrew Thomas <andrew.thomas@workday.com>
2015-12-11 16:50:01 -0800

    Adding new graphics and generation code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MazeGenerator maze = new MazeGenerator(10, 10);
-        maze.display();
+        Game game = new Game();
+        game.run();
     }
 }
--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -1,81 +1,74 @@
 import java.util.Collections;
 import java.util.Arrays;
+import java.util.Random;
 
-/*
- * recursive backtracking algorithm
- * shamelessly borrowed from the ruby at
- * http://weblog.jamisbuck.org/2010/12/27/maze-generation-recursive-backtracking
- */
 public class MazeGenerator {
-    private final int x;
-    private final int y;
-    private final int[][] maze;
+    private final int sizeX;
+    private final int sizeY;
+    private final MazeTile[][] maze;
+    Random random = new Random();
 
     public MazeGenerator(int x, int y) {
-        this.x = x;
-        this.y = y;
-        maze = new int[this.x][this.y];
-        generateMaze(0, 0);
+        sizeX = x;
+        sizeY = y;
+        maze = new MazeTile[sizeX][sizeY];
+        generateMaze();
     }
 
-    public void display() {
-        for (int i = 0; i < y; i++) {
-            // draw the north edge
-            for (int j = 0; j < x; j++) {
-                System.out.print((maze[j][i] & 1) == 0 ? "+---" : "+   ");
-            }
-            System.out.println("+");
-            // draw the west edge
-            for (int j = 0; j < x; j++) {
-                System.out.print((maze[j][i] & 8) == 0 ? "|   " : "    ");
-            }
-            System.out.println("|");
-        }
-        // draw the bottom line
-        for (int j = 0; j < x; j++) {
-            System.out.print("+---");
-        }
-        System.out.println("+");
-    }
-
-    private void generateMaze(int cx, int cy) {
-        DIR[] dirs = DIR.values();
-        Collections.shuffle(Arrays.asList(dirs));
-        for (DIR dir : dirs) {
-            int nx = cx + dir.dx;
-            int ny = cy + dir.dy;
-            if (between(nx, x) && between(ny, y)
-                    && (maze[nx][ny] == 0)) {
-                maze[cx][cy] |= dir.bit;
-                maze[nx][ny] |= dir.opposite.bit;
-                generateMaze(nx, ny);
+    private void generateMaze() {
+        for(int x = 0; x < sizeX; x++) {
+            for(int y = 0; y < sizeX; y++) {
+                maze[x][y] = new MazeTile(x, y);
             }
         }
+        //int currentX = random.nextInt(sizeX);
+        //int currentY = random.nextInt(sizeY);
+        int currentX = 5;
+        int currentY = 5;
+        visitTile(currentX, currentY, Directions.NONE);
     }
 
-    private static boolean between(int v, int upper) {
-        return (v >= 0) && (v < upper);
+    private void visitTile(int x, int y, Directions wallToRemove) {
+        maze[x][y].setVisited(true);
+        maze[x][y].setIsWallPresent(wallToRemove, false);
+
+        int nextTileToVisit = random.nextInt(4);
+
+        if(nextTileToVisit == 0) { // Visit North
+            if(y != 0 && !maze[x][y-1].getVisited()) {
+                maze[x][y].setIsWallPresent(Directions.NORTH, false);
+                visitTile(x, y-1, Directions.SOUTH);
+            }
+        }
+        if(nextTileToVisit == 1) { // Visit EAST
+            if(x != sizeX - 1 && !maze[x+1][y].getVisited()) {
+                maze[x][y].setIsWallPresent(Directions.EAST, false);
+                visitTile(x+1, y, Directions.WEST);
+            }
+        }
+        if(nextTileToVisit == 2) { // Visit SOUTH
+            if(y != sizeY - 1 && !maze[x][y+1].getVisited()) {
+                maze[x][y].setIsWallPresent(Directions.SOUTH, false);
+                visitTile(x, y+1, Directions.NORTH);
+            }
+        }
+        if(nextTileToVisit == 3) { // Visit WEST
+            if(x != 0 && !maze[x-1][y].getVisited()) {
+                maze[x][y].setIsWallPresent(Directions.WEST, false);
+                visitTile(x+1, y, Directions.EAST);
+            }
+        }
     }
 
-    private enum DIR {
-        N(1, 0, -1), S(2, 0, 1), E(4, 1, 0), W(8, -1, 0);
-        private final int bit;
-        private final int dx;
-        private final int dy;
-        private DIR opposite;
+    public int getSizeX() {
+        return sizeX;
+    }
 
-        // use the static initializer to resolve forward references
-        static {
-            N.opposite = S;
-            S.opposite = N;
-            E.opposite = W;
-            W.opposite = E;
-        }
+    public int getSizeY() {
+        return sizeY;
+    }
 
-        private DIR(int bit, int dx, int dy) {
-            this.bit = bit;
-            this.dx = dx;
-            this.dy = dy;
-        }
-    };
+    public MazeTile getMazeTileAt(int x, int y) {
+        return maze[x][y];
+    }
 }
